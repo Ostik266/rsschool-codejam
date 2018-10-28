8a5a3c8ba12842b8e783ed8f9fc6e8721f0bb9d2
Ostik266 <33750170+Ostik266@users.noreply.github.com>
2018-10-28 11:41:33 +0300

    Delete sumOfOther.js

--- sumOfOther.js
+++
@@ -1,9 +0,0 @@
-function sumOfOther(arr) {
-    let sumOfArr = arr.reduce((sum, current) => {
-        return sum + current;
-    });
-    let sumArr = arr.map(el => {
-        return sumOfArr - el;
-    });
-    return sumArr;
-}
